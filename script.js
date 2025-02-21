f27b3a297dc80e6524f9de247992a4c9ee1a1084
riku-take1 <takeiriku727@gmail.com>
2025-02-21 17:07:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ document.getElementById("omikujiButton").addEventListener("click", function () {
   document.getElementById("postToXButton").classList.remove("hidden");
 });
 
+document.getElementById('omikujiButton').addEventListener('click', function() {
+  // 講演情報の配列を作成
+  // ---公演情報ここから---
+  const items = [
+    {
+      title: "ひろゆきと10人の学生エンジニア〜論破するのか、されるのか〜",
+      time: "9月22日 17:15",
+      company: "ひろゆき",
+      hall: "A",
+    },
+    {
+      title:
+        "ビジネスに必要な全てを担い、自分の専門性を見つけ出すフルサイクル開発者のあり方",
+      time: "9月22日 16:20",
+      company: "株式会社CARTA HOLDINGS",
+      hall: "A",
+    }
+  ];
+  // ---公演情報ここまで---
+
+  // おみくじの内容をランダムに変更
+  const index = Math.floor(Math.random() * items.length);
+  // おみくじの内容を画面に表示
+  document.getElementById("omikujiTitle").textContent = items[index].title;
+  document.getElementById("omikujiDetails").textContent =
+    `${items[index].time}, ${items[index].company}, ホール: ${items[index].hall}`;
+
+  document.getElementById("postToXButton").classList.remove("hidden");
+});
